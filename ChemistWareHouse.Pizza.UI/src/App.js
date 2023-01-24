1975ba339c42f02ae848f284a8e557ecbf29f826
Bikash <bikash@stridesolutions.com.au>
2023-01-24 22:40:01 +1100

    added react project

---
+++ ChemistWareHouse.Pizza.UI/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import SideDrawer from "./components/SideBar";
+
+function App() {
+  return (
+    <div className="App">
+      <SideDrawer />
+    </div>
+  );
+}
+
+export default App;
